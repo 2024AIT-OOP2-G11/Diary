23dc861a2b3db9adce227adacecb1d2991de3c4e
Aomi-kun <yukitoaomi@gmail.com>
2024-11-28 15:32:48 +0900

    add: スズキの日記
    スズキの日記を追加しました！

---
+++ diaries/SuzukiDiary.py
@@ -0,0 +1,12 @@
+from diaries.AbstractDiary import AbstractDiary
+
+class SuzukiDiary(AbstractDiary):
+
+    def get_date(self):
+        return "2024-11-28"
+
+    def get_summary(self):
+        return "みんなで力を合わせて頑張ろう！ 困ったらいつでも相談してね！！"
+
+    def get_author(self):
+        return "K23066 鈴木"
