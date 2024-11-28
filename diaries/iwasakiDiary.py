3cbf8c37dbe73fcea549a016ac1fbd00c6368aa0
cargo-tailored <twoseamfastball1819@gmail.com>
2024-11-28 15:58:20 +0900

    Create iwasakiDiary.py

---
+++ diaries/iwasakiDiary.py
@@ -0,0 +1,12 @@
+from diaries.AbstractDiary import AbstractDiary
+
+class iwasakiDiary(AbstractDiary):
+
+    def get_date(self):
+        return "2024-11-22"
+
+    def get_summary(self):
+        return "歌い手の蛇足と【蓮】のコンビが復活した！！"
+
+    def get_author(self):
+        return "iwasaki"
