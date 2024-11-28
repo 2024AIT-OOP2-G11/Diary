7d929d2ee2308bd4f538dcd6b418c2110054cd88
yu-jimk <142887865+yu-jimk@users.noreply.github.com>
2024-11-28 15:40:30 +0900

    add:YujiDiaryの追加

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from diaries.DiarySample import DiarySample
+from diaries.YujiDiary import YujiDiary
 
 # ↓のリストには、メンバーの各日記が格納されます。
-diaries = [DiarySample(), ] 
+diaries = [DiarySample(), 
+           YujiDiary(),] 
 
 for d in diaries:
     print("---------------------------------")
